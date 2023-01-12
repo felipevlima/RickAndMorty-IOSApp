7cf89b9dfb95b9d2626e9d0dd3ee3b72141e1032
Felipe Vieira <felip.3lima@hotmail.com>
2023-01-11 22:27:39 -0300

    Inital commit

--- RIckAndMorty/Controllers/Core/RMTabViewController.swift
+++ RIckAndMorty/Controllers/Core/RMTabViewController.swift
@@ -12,7 +12,6 @@ import UIKit
 final class RMTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = .red
         setUpTabs()
     }
     
